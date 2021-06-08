fa121cca3de872b6e440e827480370667ab9d6d0
Huzefa-Jetaji <85228939+Huzefa-Jetaji@users.noreply.github.com>
2021-06-08 11:07:50 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,18 +3,12 @@ int main()
 {
     int n, i;
     for (i = 1; i <= 300; i++)
-    {
-       
-        
+    {  
        n=1;     
-        
-
         while (n++<i)
         {
             if (i % n == 0)
-            
               break;  
-            
          }
       if(n==i)
        printf("%d",i);
